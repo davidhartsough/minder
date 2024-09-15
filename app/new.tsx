88cf5f17be70fe3d9d1b1598170c8e4da465ad88
davidhartsough <hartsoughdavid@gmail.com>
2024-09-15 13:18:57 +1000

    try to do a replace then push with the router

--- app/new.tsx
+++ app/new.tsx
@@ -9,6 +9,10 @@ export default function NewListScreen() {
       pathname: "/[id]",
       params: { id },
     });
+    router.push({
+      pathname: "/[id]/notify",
+      params: { id },
+    });
   };
   return <Form onSubmit={submit} />;
 }
